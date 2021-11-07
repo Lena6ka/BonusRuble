24b2084928442e27c448ef1cc2c7ecac3437df11
Elena Tolokova <elena89-06@mail.ru>
2021-11-07 12:32:50 +0300

    Added code

--- src/main.java
+++ src/main.java
@@ -1,2 +1,14 @@
 public class main {
+    public static void main(String[] args) {
+        int initialAccount = 100;
+        int refill = 1100;
+        int bonus = 0;
+
+        if (refill >= 1000){
+            bonus = refill / 100;
+        }
+        int total = initialAccount + refill + bonus;
+
+        System.out.println("Сумма на счету " + total + " рублей");
+    }
 }
